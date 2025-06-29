c50fc62efd80f2bb2fd577e1ba212617d91d1b7b
Siphesihle Thomo <siphesihle@stoffberg.dev>
2025-06-29 17:37:45 +0200

    fix: remove pt-12 from game

--- src/app/learning/page.tsx
+++ src/app/learning/page.tsx
@@ -1,9 +1,5 @@
 import BackendDevGame from "@/components/learning/backend-challenge";
 
 export default function Learning() {
-	return (
-		<div className="pt-12">
-			<BackendDevGame />
-		</div>
-	);
+	return <BackendDevGame />;
 }
